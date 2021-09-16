4562a3c25d841dc10a913fa1f74efd8ea115438f
Raine McDevitt <raine.mcdevitt@gmail.com>
2021-09-15 20:31:28 -0500

    DONE except date bonus

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SpaDay.Models;
+
+namespace SpaDay.Controllers
+{
+    public class UserController : Controller
+    {
+        public UserController()
+        {
+        }
+
+        [HttpGet]
+        [Route("/user/add")]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("user/add")]
+        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        {
+            // add form submission handling code here
+            if (newUser.Password == verify)
+            {
+                ViewBag.username = newUser.Username;
+                return View("Index");
+            }else
+
+            {
+                ViewBag.error = "Passwords do not match. Please try again.";
+                ViewBag.email = newUser.Email;
+                ViewBag.username = newUser.Username;
+                return View("Add");
+            }
+
+            
+        }
+    }
+}
---
+++ SpaDay/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace SpaDay.Models
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        //BONUS#1: Add a Date property in User, & initialize it to the time the user joined (i.e. when the User object was created).
+        //public DateTime Date { get; set; }
+
+        public User()
+        {
+
+        }
+
+        public User(string username, string email, string password, string date):this()
+        {
+            Username = username;
+            Email = email;
+            Password = password;
+            //Date = DateTime.Now;
+        }
+    }
+}
